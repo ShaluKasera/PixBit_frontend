0cd1233718c2d14eb65393fd1a55e4cb9f80225c
ShaluKasera <shalukumari93129@gmail.com>
2025-05-09 13:21:55 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
